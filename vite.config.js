bb0142d1d7f53f6ebd7eda1cc649ddff9fd81519
Omer <feedodeedo@gmail.com>
2023-11-20 21:48:34 -0500

    changes made to package.json file

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/borrowcap-frontend/",
   server: { port: 5000 },
   test: {
     globals: true,
