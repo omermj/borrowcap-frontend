5955af5a1f7303346cfd3d90f7bce23b409ddea2
Omer <feedodeedo@gmail.com>
2023-11-21 01:16:39 -0500

    modified env variables

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.API_BASE_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:3001";
 
 /** API Class used to provide access to BorrowCap Backend */
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
