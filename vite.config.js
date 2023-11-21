97303f2fc0e2edacb960d8f078dc59810211d9bb
Omer <feedodeedo@gmail.com>
2023-11-20 22:35:48 -0500

    changes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 /// <reference types="vitest" />
 
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/borrowcap-frontend/",
+  plugins: [react()],
   server: { port: 5000 },
   test: {
     globals: true,
